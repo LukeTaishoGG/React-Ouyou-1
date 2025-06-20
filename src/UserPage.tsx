6d5b189c9a43dbb2dcd7119a8346989f1613b72a
TaishoEmmoto <luke.wood.et@gmail.com>
2025-06-20 10:46:12 +0900

    ネストされてる三項演算子をIF文に修正

--- src/UserPage.tsx
+++ src/UserPage.tsx
@@ -2,24 +2,24 @@ import { Link } from 'react-router-dom'
 import { useUsers, loadImg } from './User'
 const UserPage = () => {
   const { users, loading, error } = useUsers()
+  if (loading) {
+    return <img src={loadImg} />
+  }
+  if (error) {
+    return <p>エラー : {error}</p>
+  }
   return (
     <div className="user-page-parent">
       <div className="userName">
-        {loading ? (
-          <img src={loadImg} />
-        ) : error ? (
-          <p>エラー : {error}</p>
-        ) : (
-          <div className="user-list">
-            {users.map((user) => (
-              <div key={user.id} className="user-card">
-                <p>
-                  <Link to={`/users/${user.id}`}>{user.name}</Link>
-                </p>
-              </div>
-            ))}
-          </div>
-        )}
+        <div className="user-list">
+          {users.map((user) => (
+            <div key={user.id} className="user-card">
+              <p>
+                <Link to={`/users/${user.id}`}>{user.name}</Link>
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
